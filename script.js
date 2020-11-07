0dd8152942f2a6eb6f373c9539e0000b7b5dd24e
chelle <maphaiyarath@gmail.com>
2020-11-07 11:57:06 -0600

    able to grab location of user using ip address

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// demos
+$(document).ready(function() {
+    // find user's location based off ip address
+    var location = '';
+    var queryURL = "http://ip-api.com/json/";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        location = response.region;
+        console.log(location);
+    })
+});
